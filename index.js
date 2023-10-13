60a193d4206d7ee8879b037430f3f2be61a31514
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-10-13 10:17:47 -0400

    jfdkasl

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const DOMSelectors ={
     heading: document.getElementById("head1"),
     // or I could use querySelector instead of getElementById, if I wanted to select the object by its ID i would put # in front of the id, if i want to select
 // an object by its class I would put a . in front of the name of the class, if i want to select all objects in the class I would use querySelectorAll
-    nothing: document.querySelector(".emptydiv"),
+    form: document.querySelector("#form"),
+    first_name: document.querySelector(".first_name"),
+    first: document.querySelector(".ag-courses-item_title"),
 };
 console.log(DOMSelectors.heading);
-console.log(DOMSelectors.nothing);
 
 function changetext(text){
     text.textContent = "This text got changed"
     text.style.color = "blue"
 };
-
-changetext(DOMSelectors.heading)
+console.log(DOMSelectors.form);
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.first_name.value);
+    function(first){
+        first.textContent = DOMSelectors.first_name.value
+    }
+});
